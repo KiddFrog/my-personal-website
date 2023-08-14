e8aebd21b644417f3b94e44f39bc49a834fbcd48
domm <dommferger@gmail.com>
2023-08-14 04:54:34 -0400

    Centered text

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-
 const moveButton = document.getElementById("moveButton");
-let isMoved = false;
+const messageDiv = document.getElementById("message");
+const message = "Hello Flatiron!";
+let currentIndex = 0;
 
 moveButton.addEventListener("click", () => {
-    if (!isMoved) {
-        moveButton.style.position = "absolute";
-        moveButton.style.top = "50%";
-        moveButton.style.left = "50%";
-        moveButton.style.transform = "translate(-50%, -50%)";
-        isMoved = true;
-    } else {
-        moveButton.style.position = "static";
-        moveButton.style.top = "auto";
-        moveButton.style.left = "auto";
-        moveButton.style.transform = "none";
-        isMoved = false;
+    if (currentIndex < message.length) {
+        const nextLetter = message[currentIndex];
+        const letterSpan = document.createElement("span");
+        letterSpan.textContent = nextLetter;
+
+        // Generate a random color for the letter
+        const randomColor = getRandomColor();
+        letterSpan.style.color = randomColor;
+
+        messageDiv.appendChild(letterSpan);
+        currentIndex++;
     }
+
+    // Generate random position for the button
+    const randomX = Math.random() * (window.innerWidth - moveButton.clientWidth);
+    const randomY = Math.random() * (window.innerHeight - moveButton.clientHeight);
+
+    moveButton.style.position = "absolute";
+    moveButton.style.top = `${randomY}px`;
+    moveButton.style.left = `${randomX}px`;
 });
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
