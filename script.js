2c979df05e404756486cdc368fbd0eadf10f86ef
domm <dommferger@gmail.com>
2023-08-14 04:45:42 -0400

    added moving button

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+
+const moveButton = document.getElementById("moveButton");
+let isMoved = false;
+
+moveButton.addEventListener("click", () => {
+    if (!isMoved) {
+        moveButton.style.position = "absolute";
+        moveButton.style.top = "50%";
+        moveButton.style.left = "50%";
+        moveButton.style.transform = "translate(-50%, -50%)";
+        isMoved = true;
+    } else {
+        moveButton.style.position = "static";
+        moveButton.style.top = "auto";
+        moveButton.style.left = "auto";
+        moveButton.style.transform = "none";
+        isMoved = false;
+    }
+});
