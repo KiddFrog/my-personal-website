9a5561a6340fc51c69b13b4b85d484ea9e85155b
domm <dommferger@gmail.com>
2023-08-14 07:34:34 -0400

    close to rainbow colors and sinewave

--- script.js
+++ script.js
@@ -1,66 +1,95 @@
 const moveButton = document.getElementById('moveButton');
 const messageDiv = document.getElementById('message');
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 const messageText = 'Hello Flatiron!';
 const letters = messageText.split('');
 
-function generateRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
-}
+const letterObjects = [];
 
 let currentIndex = 0;
 
 function animateButton() {
-    let x = 0; // Initial horizontal position
-    let y = 0; // Initial vertical position
-    let xSpeed = 1; // Slower horizontal speed
-    let ySpeed = 1; // Slower vertical speed
+    let x = 0;
+    let y = 0;
+    let xSpeed = 1;
+    let ySpeed = 1;
 
-    const frameRate = 60; // Adjust the frame rate
+    const frameRate = 60;
 
     function step() {
-        // Update the button's position
         x += xSpeed;
         y += ySpeed;
 
-        // Check for collisions with the screen edges
         if (x + moveButton.clientWidth >= window.innerWidth || x <= 0) {
-            xSpeed *= -1; // Reverse horizontal direction
+            xSpeed *= -1;
         }
         if (y + moveButton.clientHeight >= window.innerHeight || y <= 0) {
-            ySpeed *= -1; // Reverse vertical direction
+            ySpeed *= -1;
         }
 
-        // Apply the new position to the button
         moveButton.style.left = x + 'px';
         moveButton.style.top = y + 'px';
 
-        // Request the next animation frame with the adjusted frame rate
         setTimeout(function() {
             requestAnimationFrame(step);
         }, 1000 / frameRate);
     }
 
-    // Start the animation loop
     step();
 }
 
-// Call the animateButton function to start the animation loop
 animateButton();
 
 moveButton.addEventListener('click', function() {
     revealNextLetter();
 });
 
+function applyCustomStyles(element, index) {
+    // Apply the sine wave effect to the letters
+    animateSinWave(element);
+
+    // Apply the rainbow color effect to the letters
+    animateRainbowColor(element, index);
+}
+
+function animateSinWave(element) {
+    const amplitude = 20;
+    const frequency = 0.15;
+
+    function step(timestamp) {
+        const verticalOffset = amplitude * Math.sin(frequency * (timestamp / 1000));
+        element.style.transform = `translateY(${verticalOffset}px)`;
+
+        requestAnimationFrame(step);
+    }
+
+    requestAnimationFrame(step);
+}
+
+function animateRainbowColor(element, index) {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    const frequency = 0.2;
+
+    function step(timestamp) {
+        const colorIndex = Math.floor((timestamp / 1000) * frequency) % colors.length;
+        element.style.color = colors[colorIndex];
+
+        requestAnimationFrame(step);
+    }
+
+    requestAnimationFrame(step);
+}
+
 function revealNextLetter() {
     if (currentIndex < letters.length) {
-        const letter = letters[currentIndex];
+        const letter = letters[currentIndex].toUpperCase();
         const span = document.createElement('span');
         span.textContent = letter;
-        span.style.color = generateRandomColor();
+
+        applyCustomStyles(span, currentIndex);
 
         messageDiv.appendChild(span);
+        letterObjects.push(span);
 
         currentIndex++;
     }
